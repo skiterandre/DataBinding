8f473ddc8176565e0b6b5b5cc58258a8920339bb
andre <skiter.andre@gmail.com>
2021-07-21 17:52:47 -0300

    Inicial commit

---
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -0,0 +1,73 @@
+package br.com.alura.ceep.ui.recyclerview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.ceep.BR
+import br.com.alura.ceep.R
+import br.com.alura.ceep.databinding.ItemNotaBinding
+import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.ui.extensions.carregaImagem
+import kotlinx.android.synthetic.main.item_nota.view.*
+
+class ListaNotasAdapter(
+    private val context: Context,
+    var onItemClickListener: (notaSelecionada: Nota) -> Unit = {}
+) : ListAdapter<Nota, ListaNotasAdapter.ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+
+        //ItemNotaBinding é o nome da classe que o compilador gera caso queira mudar o nome, adicionar a propriedade class no layout na tag data
+        val viewDataBinding = ItemNotaBinding.inflate(inflater, parent, false)
+        return ViewHolder(viewDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { nota ->
+            holder.vincula(nota)
+        }
+    }
+
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) : RecyclerView.ViewHolder(viewDataBinding.root) {
+
+        private lateinit var nota: Nota
+
+        init {
+            viewDataBinding.listener = this
+        }
+
+        fun clicaNaNota(view: View){
+            if (::nota.isInitialized) {
+                onItemClickListener(nota)
+            }
+        }
+
+        fun vincula(nota: Nota) {
+            this.nota = nota
+            viewDataBinding.nota = nota
+        }
+    }
+}
+
+object DiffCallback : DiffUtil.ItemCallback<Nota>() {
+    override fun areItemsTheSame(
+        oldItem: Nota,
+        newItem: Nota
+    ) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
+
+}
+
